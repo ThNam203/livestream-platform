97e4571bc4c4ae886aca01be074516a6d62c3050
ThNam203 <hthnam203@gmail.com>
2024-03-03 18:09:57 +0700

    get and display current user screen

--- app/(home)/livestreaming-channel-demo/page.tsx
+++ app/(home)/livestreaming-channel-demo/page.tsx
@@ -1,10 +1,51 @@
+"use client";
 import VideoJS from "@/components/video";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function LivestreamingDemo() {
+  const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
+  const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
+
+  const setupStreams = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+    });
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        noiseSuppression: true,
+        echoCancellation: true,
+        sampleRate: 44100,
+      },
+    });
+
+    setVideoStream(stream);
+    setAudioStream(audio);
+  };
+
+  const startRecording = async () => {
+    await setupStreams();
+    if (videoStream && audioStream) {
+      const mixedStream = new MediaStream([
+        ...videoStream.getTracks(),
+        ...audioStream.getTracks(),
+      ]);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4">
-      <VideoJS source="/example_video.mp4" className="w-4/5" />
+      <div data-vjs-player className="w-4/5 border">
+        {videoStream ? (
+          <VideoJS source={videoStream} />
+        ) : (
+          <div className="h-[576px] w-full flex flex-col bg-black items-center justify-center text-white">
+            <h3 className="text-3xl font-bold">You are currently offline!</h3>
+            <button className="p-2 bg-purple-500 rounded-sm mt-8" onClick={setupStreams}>Start livestream</button>
+          </div>
+        )}
+      </div>
       <div className="flex flex-row ml-4 gap-4 items-center">
         <div className="border-2 border-purple-400 rounded-full h-16 w-16 relative">
           <Image
@@ -14,7 +55,9 @@ export default function LivestreamingDemo() {
             height={64}
           />
           <div className="absolute left-0 bottom-[-2px] right-0 bg-red-600 rounded-md">
-            <p className="text-white text-xs py-[1px] font-semibold text-center">LIVE</p>
+            <p className="text-white text-xs py-[1px] font-semibold text-center">
+              LIVE
+            </p>
           </div>
         </div>
         <div className="flex flex-col">
--- components/video.tsx
+++ components/video.tsx
@@ -1,29 +1,22 @@
 "use client";
 import React, { useMemo } from "react";
 import videojs from "video.js";
+import Player from "video.js/dist/types/player";
 import "video.js/dist/video-js.css";
 
 export const VideoJS = ({
   source,
-  className,
 }: {
-  source: string;
-  className: string;
+  source: MediaStream;
 }) => {
-  const videoRef = React.useRef<any>(null);
-  const playerRef = React.useRef<any>(null);
+  const videoRef = React.useRef<HTMLDivElement>(null);
+  const playerRef = React.useRef<Player | null>(null);
   const videoJsOptions = useMemo(
     () => ({
       autoplay: true,
       controls: true,
       responsive: true,
       fluid: true,
-      sources: [
-        {
-          src: "/example_video.mp4",
-          //   type: 'video/mp4'
-        },
-      ],
     }),
     []
   );
@@ -35,7 +28,7 @@ export const VideoJS = ({
       const videoElement = document.createElement("video-js");
 
       videoElement.classList.add("vjs-big-play-centered");
-      videoRef.current.appendChild(videoElement);
+      videoRef.current!.appendChild(videoElement);
 
       const player = (playerRef.current!! = videojs(
         videoElement,
@@ -43,15 +36,14 @@ export const VideoJS = ({
         () => {}
       ));
 
+      var vid = player.tech({ IWillNotUseThisInPlugins: true }).el() as HTMLVideoElement;
+      vid.srcObject = source;
       // You could update an existing player in the `else` block here
       // on prop change, for example:
     } else {
       const player = playerRef.current;
-
-      player.autoplay(videoJsOptions.autoplay);
-      player.src(videoJsOptions.sources);
     }
-  }, [videoJsOptions, videoRef]);
+  }, [videoJsOptions, videoRef, source]);
 
   // Dispose the Video.js player when the functional component unmounts
   React.useEffect(() => {
@@ -66,9 +58,7 @@ export const VideoJS = ({
   }, [playerRef]);
 
   return (
-    <div data-vjs-player className={className}>
-      <div ref={videoRef} />
-    </div>
+      <div ref={videoRef}/>
   );
 };
 
