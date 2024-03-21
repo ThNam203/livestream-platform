acff2e5df717dd16922dfc5c2ee0630bd265e2ac
ThNam203 <hthnam203@gmail.com>
2024-03-21 14:40:09 +0700

    add backend with ffmpeg to process camera data

--- app/(home)/livestreaming-channel-demo/page.tsx
+++ app/(home)/livestreaming-channel-demo/page.tsx
@@ -1,18 +1,38 @@
 "use client";
-import VideoJS from "@/components/video";
+import { ConsumerVideoJS, StreamerVideoJS } from "@/components/video";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import socket from "@/socket"
+import videojs from "video.js";
 
 export default function LivestreamingDemo() {
   const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
-  const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
+  const [micStream, setMicStream] = useState<MediaStream | null>(null);
+  const [mixedStream, setMixedStream] = useState<MediaStream | null>(null);
+  const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
 
+  useEffect(() => {
+    function onConnect() {
+      console.log('connected');
+    }
+
+    function onDisconnect() {
+      console.log('disconnected');
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+    
+  
   const setupStreams = async () => {
-    const stream = await navigator.mediaDevices.getDisplayMedia({
+    const videoStream = await navigator.mediaDevices.getDisplayMedia({
       video: true,
-    });
-
-    const audio = await navigator.mediaDevices.getUserMedia({
       audio: {
         noiseSuppression: true,
         echoCancellation: true,
@@ -20,29 +40,72 @@ export default function LivestreamingDemo() {
       },
     });
 
-    setVideoStream(stream);
-    setAudioStream(audio);
+    const micStream = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        noiseSuppression: true,
+        echoCancellation: true,
+        sampleRate: 44100,
+      },
+    });
+
+    videoStream.getVideoTracks()[0].onended = () => {
+      alert("Your screen sharing has ended");
+      setVideoStream(null);
+      setMicStream(null);
+      setMixedStream(null);
+
+      if (recorder) {
+        recorder.stop();
+        setRecorder(null);
+      }
+    };
+
+    setVideoStream(videoStream);
+    setMicStream(micStream);
   };
 
-  const startRecording = async () => {
-    await setupStreams();
-    if (videoStream && audioStream) {
+  useEffect(() => {
+    if (videoStream && micStream) {
       const mixedStream = new MediaStream([
         ...videoStream.getTracks(),
-        ...audioStream.getTracks(),
+        ...micStream.getTracks(),
       ]);
+
+      const recorder = new MediaRecorder(mixedStream, {
+        mimeType: "video/webm",
+      });
+
+      socket.emit('config_rtmpDestination', "rtmp://10.0.188.191/live/nam");
+		  socket.emit('start','start');
+
+      recorder.ondataavailable = (event) => {
+        socket.emit("binarystream", event.data);
+      };
+
+      recorder.start(200);
+      setRecorder(recorder);
+      setMixedStream(mixedStream);
     }
+  }, [videoStream, micStream]);
+
+  const startRecording = () => {
+    setupStreams();
   };
 
   return (
     <div className="flex flex-col gap-4">
       <div data-vjs-player className="w-4/5 border">
-        {videoStream ? (
-          <VideoJS source={videoStream} />
+        {mixedStream ? (
+          <StreamerVideoJS source={mixedStream} />
         ) : (
           <div className="h-[576px] w-full flex flex-col bg-black items-center justify-center text-white">
             <h3 className="text-3xl font-bold">You are currently offline!</h3>
-            <button className="p-2 bg-purple-500 rounded-sm mt-8" onClick={setupStreams}>Start livestream</button>
+            <button
+              className="p-2 bg-purple-500 rounded-sm mt-8"
+              onClick={startRecording}
+            >
+              Start livestream
+            </button>
           </div>
         )}
       </div>
--- components/video.tsx
+++ components/video.tsx
@@ -2,15 +2,13 @@
 import React, { useMemo } from "react";
 import videojs from "video.js";
 import Player from "video.js/dist/types/player";
+import { set } from "video.js/dist/types/tech/middleware";
 import "video.js/dist/video-js.css";
 
-export const VideoJS = ({
-  source,
-}: {
-  source: MediaStream;
-}) => {
+const StreamerVideoJS = ({ source }: { source: MediaStream }) => {
   const videoRef = React.useRef<HTMLDivElement>(null);
   const playerRef = React.useRef<Player | null>(null);
+
   const videoJsOptions = useMemo(
     () => ({
       autoplay: true,
@@ -24,9 +22,7 @@ export const VideoJS = ({
   React.useEffect(() => {
     // Make sure Video.js player is only initialized once
     if (!playerRef.current) {
-      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
       const videoElement = document.createElement("video-js");
-
       videoElement.classList.add("vjs-big-play-centered");
       videoRef.current!.appendChild(videoElement);
 
@@ -36,14 +32,68 @@ export const VideoJS = ({
         () => {}
       ));
 
-      var vid = player.tech({ IWillNotUseThisInPlugins: true }).el() as HTMLVideoElement;
-      vid.srcObject = source;
+      setSrcObjectForPlayer(player, source);
       // You could update an existing player in the `else` block here
       // on prop change, for example:
+    }
+  }, [videoRef, source]);
+
+  function setSrcObjectForPlayer(vjsPlayer: Player, mediaStream: MediaStream | null) {
+    if (!mediaStream) {
+      return;
+    }
+    const videoElement = vjsPlayer.tech({}).el() as HTMLVideoElement;
+    videoElement.srcObject = null;
+    videoElement.srcObject = mediaStream;
+    vjsPlayer.play = () => videoElement.play(); // works with that fix
+    vjsPlayer.play();
+  }
+  
+
+  // Dispose the Video.js player when the functional component unmounts
+  React.useEffect(() => {
+    const player = playerRef.current;
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return <div ref={videoRef} />;
+};
+
+
+const ConsumerVideoJS = (props: any) => {
+  const videoRef = React.useRef<any>(null);
+  const playerRef = React.useRef<any>(null);
+  const {options, onReady} = props;
+
+  React.useEffect(() => {
+
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
+      const videoElement = document.createElement("video-js");
+
+      videoElement.classList.add('vjs-big-play-centered');
+      videoRef.current!.appendChild(videoElement);
+
+      const player = playerRef.current = videojs(videoElement, options, () => {
+        videojs.log('player is ready');
+        onReady && onReady(player);
+      });
+
+    // You could update an existing player in the `else` block here
+    // on prop change, for example:
     } else {
       const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
     }
-  }, [videoJsOptions, videoRef, source]);
+  }, [options, videoRef]);
 
   // Dispose the Video.js player when the functional component unmounts
   React.useEffect(() => {
@@ -58,8 +108,13 @@ export const VideoJS = ({
   }, [playerRef]);
 
   return (
-      <div ref={videoRef}/>
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
   );
 };
 
-export default VideoJS;
+export {
+  StreamerVideoJS,
+  ConsumerVideoJS
+};
